ef8e4c51eca825588c1d654ab60b14b7283e4352
Dipanshu Kakshapati <dipanshu.ksh10@gmail.com>
2024-02-23 22:24:55 +0545

    finalized streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import time
 
 # Load your trained model
 model_data = pickle.load(open('employee_performance_prediction_model.pkl', 'rb'))
@@ -9,38 +10,118 @@ model_accuracy = model_data['accuracy']
 # Assuming 'logo.png' is the path to your logo image file
 st.sidebar.image('logo.png', use_column_width=True)
 
-# Using columns to create a layout for logo next to the title
-col1, col2 = st.beta_columns([1, 8])
+# Add formatted text below the logo in the sidebar
+st.sidebar.markdown("""
+    **About the Company**  
+    *Teach for India* is a fellowship program for young professionals who want to solve the education crisis in the country. It is reported that the TFI alumni community is responsible for starting 150+ organisations in the field of social impact.
 
-with col1:
-    # Display logo in the first column
-    st.image('logo.png', width=100)
+    **Bridging the gap**  
+    *Teach for India* has approximately 900+ fellows who are deployed across 7 cities and are working with 28000 students. Many of these students do not have the reading and numeracy skills required at their grade level. The ability to predict employee performance will enable TFI to implement learning and development measures to empower their fellows to improve the reading levels of their students.
+""", unsafe_allow_html=True)
 
-with col2:
-    # Display the title in the second column
-    st.title('Employee Performance Prediction Form')
+st.title('Employee Performance Prediction Form')
 
+st.write("""
+This is a Python created an HR analytics tool that can help a talent management team in Teach For India predict employee performance and plan for their growth.
+""")
+         
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Department
+""")
 # Replace selectbox with st.radio
 department = st.selectbox('Select your department', ['Sales & Marketing', 'Operations', 'Procurement', 'Technology', 'Analytics', 'Finance', 'HR', 'R&D', 'Legal'])
 
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Education
+""")
 # Rest of your st.radio widgets replacing selectbox...
 education = st.selectbox('Select your education status', ["Bachelor's", "Master's & above", "Below Secondary", "Others"])
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Gender
+""")
 gender = st.radio('Gender', ['Male', 'Female'])
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Recruitment Channel
+""")
 recruitment_channel = st.selectbox('Select your recruitment channel', ['Other', 'Sourcing', 'Referred'])
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Awards Won
+0 means have not received an award. 1 means have received an award.
+""")
 awards_won = st.radio('Awards Won', [0, 1])
 
-# Replace number_input with st.slider
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Number of Trainigs
+""")
 no_of_trainings = st.slider('Number of Trainings', 1, 5, 1)
 
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Region
+""")
 region_options = [f'region_{i}' for i in range(1, 35)]
 region = st.selectbox('Region', options=region_options)
 
-education = st.selectbox('Education', ["Bachelor's", "Master's & above", "Below Secondary", "Others"])
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
 
+st.write("""
+### Age
+""")
 age = st.slider('Age', 20, 60, 20)
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Previous year rating 
+Rating is between 1 and 5. 1 meaning low, 5 meaning high.
+""")
 previous_year_rating = st.slider('Previous Year Rating', 1, 5, 1)
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Length of Service
+Time period is in years.
+""")
 length_of_service = st.slider('Length of Service', 1, 32, 1)
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
+
+st.write("""
+### Average Training Score
+Training score is between 1 and 100. 1 meaning low, 100 meaning high.
+""")
 avg_training_score = st.slider('Average Training Score', 1, 100, 1)
+
+# Add an empty Markdown to create more space
+st.markdown('<br>', unsafe_allow_html=True)
 
 # Processing the prediction when the user changes any input
 input_data = {
